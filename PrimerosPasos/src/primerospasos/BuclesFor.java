d9c12394702699a8e958cd8e046f4a878f5e5a92
Jonathan Moreno <jamh0207@gmail.com>
2022-01-13 11:54:02 -0400

    UPLOAD MORE INFORMATION LOOPS JAVA

--- PrimerosPasos/src/primerospasos/BuclesFor.java
+++ PrimerosPasos/src/primerospasos/BuclesFor.java
@@ -1,12 +1,38 @@
 package primerospasos;
 
-public class BuclesFor {
-    public static void main(String[] args) {
+import javax.swing.JOptionPane;
 
-        for(int i=0 ; i<10 ; i++){
-            System.out.println("Jonathan");
+public class BuclesFor {
+    
+    public static void main(String[] args) {
+        
+        //COMPROBAR MAIL
+        
+        int arroba = 0;
+        
+        boolean punto=false;
+        
+        String mail = JOptionPane.showInputDialog("Introduce tu mail");
+        
+        for(int i=0; i<mail.length(); i++){
             
+            if(mail.charAt(i)== '@'){
+                
+                arroba++;
+            }
+            if(mail.charAt(i)== '.'){
+                
+                punto = true;
+            }
+        
         }
+        if(arroba == 1 && punto==true){
+            System.out.println("El mail es correcto");
+        }
+        else{
+            System.out.println("El mail es incorrecto");
+        }
+            
     }
 }
 
